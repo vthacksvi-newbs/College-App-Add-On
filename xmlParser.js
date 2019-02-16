22d833d98bcfd400fbd12a00818c94505c7f73fc
randor12 <43661011+randor12@users.noreply.github.com>
2019-02-16 15:07:56 -0500

    Add files via upload

--- xmlParser.js
+++ xmlParser.js
@@ -31,5 +31,19 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     fileChooser.click();
     sendResponse({ response: "fileChooser clicked" });
   }
+  else if(request.message == "Submit") {
+    var request = new XMLHttpRequest();
+    request.open("GET", FormData, false);
+    request.send();
+    var xml = request.responseXML;
+    var users = xml.getElementsByTagName("user");
+    for (var i = 0; i < users.length; i++) {
+      var user = users[i];
+      var names = user.getElementsByTagName("name");
+      for (var j = 0; j < names.length; j++) {
+        alert(names[j].childNodes[0].nodeValue);
+      }
+    }
+  }
 
 });

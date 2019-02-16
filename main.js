5e76db70fb457cf7176b06f6aba112a8495d203a
Marco Sirabella <marco@sirabella.org>
2019-02-16 16:35:36 -0500

    Add options page redirect

--- main.js
+++ main.js
@@ -3,4 +3,26 @@ function main() {
 		setValue(id, extractXmlValue(id));
 	})
 }
+function onUploadFile(file_input) {
+	var file = file_input.files[0]
 
+	const reader = new FileReader();
+
+	// This fires after the blob has been read/loaded.
+	reader.addEventListener('loadend', (e) => {
+		browser.storage.sync.set({
+			"name": file.name,
+			"file": e.target.result
+		})
+	});
+
+
+	reader.readAsText(file.slice())
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("hi")
+	browser.tabs.create({
+		url: "/fullscreen.html"
+	})
+});

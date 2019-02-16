5456b3d5d1e1d16838ced587b9ca59cb786600e8
Marco Sirabella <marco@sirabella.org>
2019-02-16 11:45:08 -0500

    Added framework calls for commonapp

---
+++ commonapp/lookup.js
@@ -0,0 +1,13 @@
+function extractXmlValues(id) {
+	return {
+		"container_ques_173": function() { // first name
+			return "Marco"
+		},
+		"container_ques_174": function() { // middle name
+			return "Joseph"
+		},
+		"container_ques_175": function() { // middle name
+			return "Sirabella"
+		},
+	}[id]
+}
--- commonapp/setvals.js
+++ commonapp/setvals.js
@@ -1,15 +1,13 @@
-function setdropdowntovalue(n, v) {
-	var selector = 'select[name^="Controls[' + n + '"]'
-	var actual_dropdown = $(selector)[0]
+function SetValue_dropdown(d, v) {
+	var actual_dropdown = d.querySelector("select")
 	actual_dropdown.value = v
+
 	$._data($(actual_dropdown)[0]).events["liszt:updated"][0].handler()
-	var input = $(selector + ' + div input')[0]
-	input.dispatchEvent(new KeyboardEvent("keyup", {
-		keyCode: 13,
-		bubbles: true,
-		cancelable: true,
-		which: 13,
-	}))
+
+	var input = d.querySelector(".chzn-search > input")
+
+	input.dispatchEvent(new KeyboardEvent("keyup", {keyCode: 32}))
+
 	input.dispatchEvent(new KeyboardEvent("keyup", {
 		keyCode: 13,
 		bubbles: true,
@@ -18,3 +16,29 @@ function setdropdowntovalue(n, v) {
 	}))
 }
 
+function SetValue_radio(d, v) {
+	var buttons = d.querySelectorAll("input[type=radio]") 
+	for (var i in buttons) {
+		var rb = buttons[i]
+		var label = d.querySelector("label[for=" + rb.id + "]")
+		if (label.innerText == v) {
+			rb.click()
+			return
+		}
+	}
+}
+
+function SetValue_short(d, v) {
+	d.querySelector("input.input").value = v
+}
+
+function setValue(id, v) {
+	var elem = document.getElementById(id)
+	if (elem.classList.contains("short")) {
+		return SetValue_short(elem, v)
+	} else if (elem.classList.contains("singleSelect")) {
+		return SetValue_dropdown(elem, v)
+	} else if (elem.classList.contains("singleSelectRadio")) {
+		return SetValue_radio(elem, v)
+	}
+}

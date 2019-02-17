85dcca35edf1c55f36b7264ec002d729f3ae5db6
Alisa Khuu <alisakhuu@gmail.com>
2019-02-17 05:54:33 -0500

    commented out background.js

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 //Background JavaScript file
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.message == "import") {
-        fields = request.fields; // use the data
-        sendResponse({ response: "imported" });
-    }
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    // if (request.message == "import") {
+        // fields = request.fields; // use the data
+		// Browser.storage.StorageArea.set({file:fields});
+        // sendResponse({ response: "imported" });
+    // }
+	
 
     
-});
+// });
 

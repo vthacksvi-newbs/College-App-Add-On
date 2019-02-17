3605aaaa27a9838cc69c17ee38f3409f37fbac98
Marco Sirabella <marco@sirabella.org>
2019-02-17 07:04:11 -0500

    But not for chrome...

--- commonapp/lookup.js
+++ commonapp/lookup.js
@@ -2,7 +2,12 @@ function extractXmlValue(xml, id) {
 	var responseFunc = {
 		"container_ques_173": function() { // first name
 			//return getxmlvalue(me.name.first)
-			return xml.querySelector("user_info > me > name > first").textContent
+			console.log(xml.querySelector("user_info"))
+			console.log(xml.querySelector("user_info").querySelector("me").querySelector("name").querySelector("first"))
+			console.log(xml.querySelector("user_info").querySelector("me").querySelector("name").querySelector("first").nodeValue)
+			console.log(xml.querySelector("user_info").querySelector("me").querySelector("name").querySelector("first").innerText)
+			console.log(xml.querySelector("user_info").querySelector("me").querySelector("name").querySelector("first").textContent)
+			return xml.querySelector("user_info").querySelector("me").querySelector("name").querySelector("first").textContent
 		},
 		"container_ques_174": function() { // middle name
 			return "middlename"

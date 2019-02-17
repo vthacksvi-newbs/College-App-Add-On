2a11656b17a32f2a5cce2825e3022eee669a0d6b
Marco Sirabella <marco@sirabella.org>
2019-02-17 06:00:09 -0500

    Fix syntax

--- ui.js
+++ ui.js
@@ -2,7 +2,7 @@
 // when there is a file present or not
 // Can also set the theme of the Popup if time is alotted
 
-file.addEventListener("change", = () => {
+file.addEventListener("change", () => {
     var img = document.getElementById("image");
     img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Checkmark_green.svg/1180px-Checkmark_green.svg.png";
     console.log(img.src);

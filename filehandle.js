1f55709ad65a8a5dc5dc69b25d0a138e998050a1
Alisa Khuu <alisakhuu@gmail.com>
2019-02-17 06:02:00 -0500

    created filehandle.js for parsing xml

---
+++ filehandle.js
@@ -0,0 +1,13 @@
+function toText() {
+	var file = document.getElementById("file").files[0]; //for input type=file
+	var reader=new FileReader();
+	reader.onload = function(e) {}
+	reader.readAsText(file);
+	var error = reader.error;
+	var texte=reader.result;
+	parser = new DOMParser();
+	xmlDoc = parser.parseFromString(texte,"text/xml");
+	console.log(texte);
+}
+
+file.addEventListener("change", toText);

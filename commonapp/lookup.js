a0dfa536c969ae340a5655dcf6f3c04819b61481
Marco Sirabella <marco@sirabella.org>
2019-02-17 06:53:27 -0500

    Moar debug

--- commonapp/lookup.js
+++ commonapp/lookup.js
@@ -2,6 +2,10 @@ function extractXmlValue(xml, id) {
 	var responseFunc = {
 		"container_ques_173": function() { // first name
 			//return getxmlvalue(me.name.first)
+			console.log(xml.querySelector("user_info"))
+			console.log(xml.querySelector("user_info").querySelector("me").querySelector("name").querySelector("first"))
+			console.log(xml.querySelector("user_info").querySelector("me").querySelector("name").querySelector("first").nodeValue)
+			console.log(xml.querySelector("user_info").querySelector("me").querySelector("name").querySelector("first").innerText)
 			return xml.querySelector("user_info").querySelector("me").querySelector("name").querySelector("first").innerText
 		},
 		"container_ques_174": function() { // middle name
